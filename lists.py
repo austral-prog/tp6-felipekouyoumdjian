36fe5b8e349761249d3080964f4b0a6abfcf641f
felipekouyoumdjian <fkouyoumdjian@mail.austral.edu.ar>
2025-11-19 09:21:35 -0300

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,20 +1,38 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    length = len(list_to_remove_elements)
+    if length == 0:
+        return []
+    elif length < 5:
+        return list_to_remove_elements[1:]
+    elif length == 5:
+        return list_to_remove_elements[1:4]
+    else:
+        return list_to_remove_elements[1:4] + list_to_remove_elements[6:]
+print(remove_elements(['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow', 'Blue']))
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    return ["Pink"] + list_to_add_elements + ["Yellow"]
+print(add_elements(['Red', 'Green', 'White', 'Black']))
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_check) == 0:
+        return True
+    return False
+print(is_empty([]))
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_compare1) < 3 or len(list_to_compare2) < 3:
+        return False
+    else:
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        return False
+print(check_lists(['Black', 'Pink', 'Yellow', 'Red', 'Green', 'White'], 
+                  ['Red', 'Green', 'Yellow', 'White', 'Black', 'Pink']))
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    new_list = [list_of_lists_to_modify[0][:2], list_of_lists_to_modify[1][1:4], list_of_lists_to_modify[2][-2:]]
+    return new_list
+print(list_of_lists([[1, 2, 3], [4, 5, 6, 7, 8], [9, 10, 11, 12]]))
